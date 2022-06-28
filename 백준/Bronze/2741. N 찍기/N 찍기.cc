c70293cd9b6eaaa8204ab1f9d7361ecd8a39b820
Sangjin <81291090+skmn3@users.noreply.github.com>
2022-06-28 16:27:44 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(int argc, char const *argv[]) {
+ 
+	int N;
+	cin >> N;
+ 
+	for (int i = 1; i <= N; i++) {
+		cout << i << "\n";
+	}
+	return 0;
+}
