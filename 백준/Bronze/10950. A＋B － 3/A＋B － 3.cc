0f329f9240eafb11bc79e2b36225971eefa59160
Sangjin <81291090+skmn3@users.noreply.github.com>
2022-06-28 16:14:04 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main(void){
+    int n, a, b;
+    cin>>n;
+    for(int i = 0; i < n; i++){
+        cin>>a>>b;
+        cout << a + b <<endl;
+    }
+}
