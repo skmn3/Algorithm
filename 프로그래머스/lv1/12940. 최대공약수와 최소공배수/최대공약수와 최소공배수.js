137cf0db05f8ea8bfbca3c63084fba1bc05c3a71
Sangjin <sangjin170202@naver.com>
2022-09-26 16:54:49 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,5 @@
+function solution(n, m) { // 유클리드 호제법
+    const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
+    const lcm = (a, b) => a * b / gcd(a, b);
+    return [gcd(n, m), lcm(n, m)];
+}
