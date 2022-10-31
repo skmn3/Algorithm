64616d6a1f86d43afb521c4f81b65c8217682355
Sangjin <sangjin170202@naver.com>
2022-10-31 17:00:22 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,14 +1,14 @@
 function solution(number, k) {
     const arr = [];
     for(let i = 0; i < number.length; i ++) {
-      (function recur() {
-        if(k > 0 && arr[arr.length-1] < number[i]) {
-            arr.pop();
-            k--;
-            recur()
-         }
-      })()  
-      arr.push(number[i]);
+        (function re() {
+            if(k > 0 && arr[arr.length-1] < number[i]) {
+                arr.pop();
+                k--;
+                re()
+            }
+        })()
+        arr.push(number[i]);
     }  
-    return arr.slice(0,arr.length-k).join('');  
+    return arr.slice(0, arr.length-k).join('');  
 }
