9affe3a32d35e7093716653f7a9dd13a9891ce20
Sangjin <sangjin170202@naver.com>
2022-10-31 19:22:57 +0900

    [level 2] Title: 오픈채팅방, Time: 132.59 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,20 @@
+function solution(record) {
+    const answers = [];
+    const userList = {};
+    
+    const joinMsg = {
+        Enter: "들어왔습니다",
+        Leave: "나갔습니다"
+    };
+    
+    for (let records of record) {
+        const [ answer, uid, nickname ] = records.split(' ');
+        if (nickname) {
+            userList[uid] = nickname;
+        }
+        if (answer !== "Change") {
+            answers.push([answer, uid]);
+        }
+    }
+    return answers.map(([answer, uid]) => `${userList[uid]}님이 ${joinMsg[answer]}.`);
+}
