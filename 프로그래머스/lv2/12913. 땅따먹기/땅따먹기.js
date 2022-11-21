d9d5761cd927c5a528ce7da1f77932e033a90e26
Sangjin <sangjin170202@naver.com>
2022-11-21 19:49:19 +0900

    [level 2] Title: 땅따먹기, Time: 19.71 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,14 @@
+function solution(land) {
+    var answer = '';
+    const length = land.length;
+    
+    for (var i = 1; i < length; i++) {
+        land[i][0] += Math.max(land[i-1][1], land[i-1][2], land[i-1][3]);
+        land[i][1] += Math.max(land[i-1][0], land[i-1][2], land[i-1][3]);
+        land[i][2] += Math.max(land[i-1][0], land[i-1][1], land[i-1][3]);
+        land[i][3] += Math.max(land[i-1][0], land[i-1][1], land[i-1][2]);
+    }
+    
+    answer = land[land.length - 1];
+    return Math.max(...answer);
+}
