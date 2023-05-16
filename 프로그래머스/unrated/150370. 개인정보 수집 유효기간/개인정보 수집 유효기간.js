9d1ac0e51cd9f16b66c3686006fa34115c555c37
Sangjin <sangjin170202@naver.com>
2023-05-16 19:17:23 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.56 ms, Memory: 34.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,24 @@
+function solution(today, terms, privacies) {
+    const answer = [];
+    
+    const expire = new Date(today);
+    const termType = {};
+    
+    terms.forEach((item) => {
+        const [type, term] = item.split(" ");
+        termType[type] = Number(term);
+    });
+    
+    privacies.forEach((item, idx) => {
+        const [date, type] = item.split(" ");
+        
+        const chDate = new Date(date);
+        
+        chDate.setMonth(chDate.getMonth() + termType[type]);
+        
+        if (chDate <= expire) answer.push(idx + 1);
+        
+    })
+    
+    return answer;
+}
